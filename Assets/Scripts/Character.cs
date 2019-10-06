c17d8bcad274f56f7c1aca2c6b326e95c11a1e6d
Александр Тарасенко <aleksandrtarasenko@MacBook-Air-Aleksandr.local>
2019-10-07 00:39:24 +0300

    Music & Character audio

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -16,31 +16,49 @@ public class Character : MonoBehaviour
     public Transform punch1;
     public float punch1Radius = 0.75f;
     [SerializeField] private int damage = 5;
+    public Camera cam;
+    
 
 
 
     void Start()
     {
+
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        
     }
 
     void FixedUpdate()
     {
-       
+        cam.transform.position = rb.position;
         float move = Input.GetAxis("Horizontal");
         anim.SetFloat("Run", Mathf.Abs(move));
         rb.velocity = new Vector2(move * speed, rb.velocity.y);
         if (move > 0 && !isFacingRight)
+        {
             Flip();
+            SoundManager.instance.RandomizeSfx(moveSound);
+        }
+            
+
         else if (move < 0 && isFacingRight)
+        {
             Flip();
+            SoundManager.instance.RandomizeSfx(moveSound);
+        }
+          
+       
 
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             Jump();
+            SoundManager.instance.RandomizeSfx(jumpSound);
+        }
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Atack();
+            SoundManager.instance.RandomizeSfx(attackSound);
         }
     }
     private void Flip()
