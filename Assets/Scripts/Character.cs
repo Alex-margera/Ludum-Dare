34f458af0a0f17b43225c547093e1471e31ac51c
Александр Тарасенко <aleksandrtarasenko@MacBook-Air-Aleksandr.local>
2019-10-06 21:48:33 +0300

    first commit

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Character : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    [SerializeField] private float speed = 10;
+    public AudioClip moveSound;
+    public AudioClip jumpSound;
+    public AudioClip attackSound;
+    Animator anim;
+    private bool isFacingRight = true;
+    public Transform punch1;
+    public float punch1Radius = 0.75f;
+    [SerializeField] private int damage = 5;
+
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+       
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("Run", Mathf.Abs(move));
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+        if (move > 0 && !isFacingRight)
+            Flip();
+        else if (move < 0 && isFacingRight)
+            Flip();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Atack();
+        }
+    }
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector2 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    public void Jump()
+    {
+        rb.AddForce(Vector2.up * 5000);
+        anim.SetTrigger("Jump");
+     
+    }
+
+    public void Atack()
+    {
+        Attack.Action(punch1.position, punch1Radius, 8, damage, true);
+        anim.SetTrigger("Attack");
+    }
+
+
+   
+}
---
+++ Assets/Scripts/PlatformGeneration.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGeneration : MonoBehaviour
+{
+    [SerializeField] private GameObject platform;
+    private int completeLevels = 5;
+
+    public void Start()
+        {
+        StartCoroutine(OnGeneratingRoutine());
+        }
+
+    private IEnumerator OnGeneratingRoutine()
+    {
+        int count = this.completeLevels + 5;
+        for (int i = 0; i < count; i++)
+        {
+            Vector2 size = new Vector2(1, 1);
+            Vector2 position = new Vector2(0, 0);
+            platform.transform.position = position;
+            platform.transform.localScale = size;
+            position.x += size.x;
+            position.y += size.y * Random.Range(-1, 2);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public void CompleteLevel()
+        {
+            this.completeLevels += 1;
+            StartCoroutine(OnGeneratingRoutine());
+        }
+
+    }
